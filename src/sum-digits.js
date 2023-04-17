4d9080b98622ca7afbe2cc4e4fede3c6985182e1
Sergey <segany4@gmail.com>
2023-04-18 02:01:48 +0300

    done sum-digits

--- src/sum-digits.js
+++ src/sum-digits.js
@@ -1,7 +1,10 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
 function getSumOfDigits(n) {
-
+  while (n.toString().length >= 2) {
+    n = n.toString().split('').reduce((a, b) => Number(a) + Number(b))
+  }
+  return n
 }
 
 module.exports = {
